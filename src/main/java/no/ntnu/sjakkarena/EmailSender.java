b5ec684f3d452d295dde7e2e75a8f1a841704df4
andremha <andremha@stud.ntnu.no>
2020-02-11 12:24:45 +0100

    Add email sender

---
+++ src/main/java/no/ntnu/sjakkarena/EmailSender.java
@@ -0,0 +1,42 @@
+package no.ntnu.sjakkarena;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+
+@Component
+public class EmailSender {
+
+    private JavaMailSender emailSender;
+
+    public EmailSender(){
+        // Code from https://www.baeldung.com/spring-email
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("sjakkarena@gmail.com");
+        mailSender.setPassword("Sjakkarena1234");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        emailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String text){
+        // Code from https://www.baeldung.com/spring-email
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
