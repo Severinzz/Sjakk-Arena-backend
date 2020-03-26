97507fd273641e9bedbc69aaf0ac41eaf8086600
JuleNissen <jahn_willy97@hotmail.com>
2020-03-26 13:57:25 +0100

    gamerestcontroller add invalidresults getter

--- src/main/java/no/ntnu/sjakkarena/restcontrollers/GameRESTController.java
+++ src/main/java/no/ntnu/sjakkarena/restcontrollers/GameRESTController.java
@@ -2,6 +2,7 @@ package no.ntnu.sjakkarena.restcontrollers;
 
 import no.ntnu.sjakkarena.data.ResultUpdateRequest;
 import no.ntnu.sjakkarena.exceptions.NotInDatabaseException;
+import no.ntnu.sjakkarena.repositories.GameRepository;
 import no.ntnu.sjakkarena.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,6 +32,15 @@ public class GameRESTController {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (IllegalArgumentException e){
             return new ResponseEntity<>("Not a valid result", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "tournament/invalidGames", method = RequestMethod.GET)
+    public ResponseEntity<String> invalidResult(@PathVariable(name = "invalidResultGame") int tournamentID) {
+        try {
+            GameRepository.getInvalidResultGames(tournamentID);
+        } catch (NotInDatabaseException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
