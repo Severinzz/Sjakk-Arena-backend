46c6554ef9ac695cde31ed1592df978f4179be15
andremha <andremha@stud.ntnu.no>
2020-04-21 10:50:51 +0200

    Stop requests when tournament is inactive

--- src/main/java/no/ntnu/sjakkarena/config/WebConfig.java
+++ src/main/java/no/ntnu/sjakkarena/config/WebConfig.java
@@ -1,28 +1,36 @@
 package no.ntnu.sjakkarena.config;
 
+import no.ntnu.sjakkarena.AccessControlInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
-// Code from https://github.com/rstoyanchev/spring-websocket-portfolio
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AccessControlInterceptor accessControlInterceptor;
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
 
+    // Adapted from https://github.com/rstoyanchev/spring-websocket-portfolio
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/")
                 .resourceChain(false)
                 .addResolver(new WebJarsResourceResolver())
                 .addResolver(new PathResourceResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accessControlInterceptor);
     }
 }
