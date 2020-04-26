2455ead156a147b9fb0c78109787b97df92790bb
andremha <andremha@stud.ntnu.no>
2020-04-26 17:51:47 +0200

    Add comments to EventCreators

--- src/main/java/no/ntnu/sjakkarena/eventcreators/GameEventCreator.java
+++ src/main/java/no/ntnu/sjakkarena/eventcreators/GameEventCreator.java
@@ -14,6 +14,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * Creates Game Events.
+ */
 @Component
 public class GameEventCreator {
 
@@ -26,12 +29,22 @@ public class GameEventCreator {
     @Autowired
     protected ApplicationEventPublisher applicationEventPublisher;
 
+    /**
+     * Creates and publishes an event where games have been created
+     *
+     * @param tournamentId The id of the tournament which the newly created games belong to
+     */
     public void createAndPublishGamesCreatedEvent(int tournamentId) {
-        List<? extends Game> activeGames =  gameWithPlayerNamesRepository.getActiveGames(tournamentId);
+        List<? extends Game> activeGames = gameWithPlayerNamesRepository.getActiveGames(tournamentId);
         GamesCreatedEvent gamesCreatedEvent = new GamesCreatedEvent(this, activeGames, tournamentId);
         applicationEventPublisher.publishEvent(gamesCreatedEvent);
     }
 
+    /**
+     * Creates and publishes an event where a valid result is added
+     *
+     * @param gameId The game which the valid result is associated with
+     */
     public void createAndPublishValidResultAddedEvent(int gameId) {
         Game game = gameWithPlayerNamesRepository.getGame(gameId);
         Player whitePlayer = playerRepository.getPlayer(game.getWhitePlayerId());
@@ -40,14 +53,25 @@ public class GameEventCreator {
                 whitePlayer.getTournamentId()));
     }
 
+    /**
+     * Creates and publishes an event where a result has been suggested by a player
+     *
+     * @param opponentId The id of the player who will receive the result suggestion
+     * @param result     The result being suggested
+     * @param gameId     The id of the game the suggested result is associated with
+     */
     public void createAndPublishResultSuggestedEvent(int opponentId, double result, int gameId) {
         applicationEventPublisher.publishEvent(new ResultSuggestedEvent(this, gameId, opponentId, result));
     }
 
-    public void createAnPublishInvalidResultEvent(int gameId){
+    /**
+     * Creates and publishes an event where a result has been declared invalid
+     *
+     * @param gameId The id of the game the invalid result is associated with
+     */
+    public void createAnPublishInvalidResultEvent(int gameId) {
         Game game = gameWithPlayerNamesRepository.getGame(gameId);
         Player player = playerRepository.getPlayer(game.getWhitePlayerId());
         applicationEventPublisher.publishEvent(new InvalidResultEvent(this, game, player.getTournamentId()));
     }
-
 }
