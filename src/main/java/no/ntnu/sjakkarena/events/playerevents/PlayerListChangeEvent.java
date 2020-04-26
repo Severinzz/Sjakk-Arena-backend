4dead7f421f07d6e3406982ae6a64f6ce714e57f
andremha <andremha@stud.ntnu.no>
2020-04-26 18:33:03 +0200

    Add comments to events (+ remove tournamentId from ValidResultAddedEvent)

--- src/main/java/no/ntnu/sjakkarena/events/playerevents/PlayerListChangeEvent.java
+++ src/main/java/no/ntnu/sjakkarena/events/playerevents/PlayerListChangeEvent.java
@@ -4,6 +4,10 @@ import no.ntnu.sjakkarena.data.Player;
 import org.springframework.context.ApplicationEvent;
 
 import java.util.List;
+
+/**
+ * An event where a change in a tournament's list of players has occurred. This event is classified as a player event
+ */
 public class PlayerListChangeEvent extends ApplicationEvent {
 
     List<Player> players;
@@ -14,6 +18,16 @@ public class PlayerListChangeEvent extends ApplicationEvent {
 
     private int tournamentId;
 
+    /**
+     * Constructs an PlayerListChangeEvent
+     *
+     * @param source The object on which the Event initially occurred.
+     *       (description from https://docs.oracle.com/javase/8/docs/api/java/util/EventObject.html)
+     * @param players List of players in the tournament to which the player is added
+     * @param leaderBoard The leader board in the tournament to which the player is added
+     * @param tournamentHasStarted Whether the tournament to which the player is added has started
+     * @param tournamentId The id of the tournament to which the player is added
+     */
     public PlayerListChangeEvent(Object source, List<Player> players, List<Player> leaderBoard,
                                  boolean tournamentHasStarted, int tournamentId) {
         super(source);
