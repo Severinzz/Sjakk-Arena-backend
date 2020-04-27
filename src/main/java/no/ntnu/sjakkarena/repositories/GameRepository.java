848a4ab3f01767928099988a64b2b1a4b904db9d
andremha <andremha@stud.ntnu.no>
2020-04-27 14:28:06 +0200

    Add comments to repositories

--- src/main/java/no/ntnu/sjakkarena/repositories/GameRepository.java
+++ src/main/java/no/ntnu/sjakkarena/repositories/GameRepository.java
@@ -16,6 +16,9 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Repository containing methods for handling game data
+ */
 @Repository
 public class GameRepository {
 
@@ -25,26 +28,32 @@ public class GameRepository {
     private RowMapper<Game> gameRowMapper = new GameRowMapper();
 
     /**
-     * Gets an active game where the provided players are playing
+     * Gets an active game where the two players are playing
      *
-     * @param player1
-     * @param player2
-     * @return The active game where the provided players are playing
+     * @param player1Id The id of one of the playing players
+     * @param player2Id The id of the other playing player
+     * @return The active game where the players are playing
      */
-    public Game getActiveGame(int player1, int player2) {
+    public Game getActiveGame(int player1Id, int player2Id) {
         try {
             return jdbcTemplate.queryForObject("SELECT * " +
                     "FROM `sjakkarena`.`game` " +
                     "WHERE (`active` = 1) " +
-                    "AND ((white_player = " + player1 + " " +
-                    "AND  black_player = " + player2 + ")" +
-                    "OR (white_player = " + player2 + " " +
-                    "AND black_player = " + player1 + "))", gameRowMapper);
+                    "AND ((white_player = " + player1Id + " " +
+                    "AND  black_player = " + player2Id + ")" +
+                    "OR (white_player = " + player2Id + " " +
+                    "AND black_player = " + player1Id + "))", gameRowMapper);
         } catch (IncorrectResultSizeDataAccessException e) {
             throw new NotInDatabaseException("Player has no active games");
         }
     }
 
+    /**
+     * Gets a player's active game
+     *
+     * @param playerId The id of the player
+     * @return A player's active game.
+     */
     public Game getActiveGame(int playerId) {
         try {
             return jdbcTemplate.queryForObject("SELECT * " +
@@ -58,74 +67,93 @@ public class GameRepository {
     }
 
     /**
-     * Add a game result to the database
+     * Adds a game result to the database
      *
-     * @param gameId The game id
-     * @param whitePlayerPoints The game result
+     * @param gameId            The id of the game the result is associated with
+     * @param whitePlayerPoints The number of points the white player received
      */
     public void addResult(int gameId, double whitePlayerPoints) {
         String end = LocalDateTime.now().toString();
-        int affectedRows = jdbcTemplate.update("UPDATE `sjakkarena`.`game` SET `white_player_points` = " + whitePlayerPoints  + ", " +
+        int affectedRows = jdbcTemplate.update("UPDATE `sjakkarena`.`game` SET `white_player_points` = " + whitePlayerPoints + ", " +
                 " `end` = \"" + end + "\" WHERE game_id = " + gameId);
-        if (affectedRows != 1){
+        if (affectedRows != 1) {
             throw new TroubleUpdatingDBException("Some problems occurred while trying to make result valid");
         }
     }
 
+    /**
+     * Adds multiple games to the database
+     *
+     * @param games The games to be added
+     */
     //Adapted code from https://www.baeldung.com/spring-jdbc-jdbctemplate
-    public void addGames(List<Game> newGames) {
-         jdbcTemplate.batchUpdate("INSERT INTO `sjakkarena`.`game` (`table`, `start`, `white_player`, " +
+    public void addGames(List<Game> games) {
+        jdbcTemplate.batchUpdate("INSERT INTO `sjakkarena`.`game` (`table`, `start`, `white_player`, " +
                         "`black_player`, `active`) VALUES (?, ?, ?, ?, ?)",
                 new BatchPreparedStatementSetter() {
                     @Override
                     public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setInt(1, newGames.get(i).getTable());
-                        ps.setString(2, newGames.get(i).getStart());
-                        ps.setInt(3, newGames.get(i).getWhitePlayerId());
-                        ps.setInt(4, newGames.get(i).getBlackPlayerId());
-                        ps.setBoolean(5, newGames.get(i).isActive());
+                        ps.setInt(1, games.get(i).getTable());
+                        ps.setString(2, games.get(i).getStart());
+                        ps.setInt(3, games.get(i).getWhitePlayerId());
+                        ps.setInt(4, games.get(i).getBlackPlayerId());
+                        ps.setBoolean(5, games.get(i).isActive());
                     }
+
                     @Override
                     public int getBatchSize() {
-                        return newGames.size();
+                        return games.size();
                     }
                 });
     }
 
 
     /**
-     * Sets a games valid state to invalid
-     * @param gameID of game to make invalidvalid.
+     * Sets a game's result to invalid
+     *
+     * @param gameID The id of the game which will have an invalid result.
      */
-    public void makeResultInvalid(int gameID){
-        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET valid_result = 0 "+
-                "WHERE game_id = " +gameID);
-        if (affectedRows != 1){
+    public void makeResultInvalid(int gameID) {
+        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET valid_result = 0 " +
+                "WHERE game_id = " + gameID);
+        if (affectedRows != 1) {
             throw new TroubleUpdatingDBException("Some problems occurred while trying to make result invalid");
         }
     }
 
     /**
-     * Sets a games valid state to valid
-     * @param gameID of game to make valid.
+     * Sets a game's result to valid
+     *
+     * @param gameID The id of the game which will have a valid result.
      */
-    public void makeResultValid(int gameID){
-        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET valid_result = 1 "+
-                "WHERE game_id = " +gameID);
-        if (affectedRows != 1){
+    public void makeResultValid(int gameID) {
+        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET valid_result = 1 " +
+                "WHERE game_id = " + gameID);
+        if (affectedRows != 1) {
             throw new TroubleUpdatingDBException("Some problems occurred while trying to make result valid");
         }
     }
 
-    public void deactivateGame(int gameId){
-        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET `active` = 0 "+
-            "WHERE game_id = " + gameId);
-        if (affectedRows != 1){
+    /**
+     * Deactivates a game
+     *
+     * @param gameId The id of the game to be deactivated
+     */
+    public void deactivateGame(int gameId) {
+        int affectedRows = jdbcTemplate.update("UPDATE sjakkarena.game SET `active` = 0 " +
+                "WHERE game_id = " + gameId);
+        if (affectedRows != 1) {
             throw new TroubleUpdatingDBException("Some problems occurred while trying to deactivate game");
         }
     }
 
+    /**
+     * Gets a game by the game's id
+     *
+     * @param gameId The id of the game to be returned
+     * @return a game
+     */
     public Game getGame(int gameId) {
-        return jdbcTemplate.queryForObject("SELECT * FROM sjakkarena.game WHERE game_id = " +gameId, gameRowMapper);
+        return jdbcTemplate.queryForObject("SELECT * FROM sjakkarena.game WHERE game_id = " + gameId, gameRowMapper);
     }
 }
