46805f98f011324d1d4b1112c627b06bfc0b2003
JuleNissen <jahn_willy97@hotmail.com>
2020-04-30 16:00:26 +0200

    Returns 200 if upload ok, 403 if no jwt

--- src/main/java/no/ntnu/sjakkarena/controllers/FileUploadController.java
+++ src/main/java/no/ntnu/sjakkarena/controllers/FileUploadController.java
@@ -2,12 +2,17 @@ package no.ntnu.sjakkarena.controllers;
 
 import no.ntnu.sjakkarena.services.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+// code adapted from: https://www.devglan.com/spring-boot/spring-boot-file-upload-download
 
 @RestController
 public class FileUploadController {
@@ -15,7 +20,13 @@ public class FileUploadController {
     FileUploadService fileUploadService;
 
     @PostMapping("/playerFile/upload")
-    public void uploadFile(@RequestParam("file")MultipartFile file) throws IOException {
-        fileUploadService.uploadFile(file);
+    public ResponseEntity uploadFile(@RequestParam("file")MultipartFile file) throws IOException {
+        try {
+            fileUploadService.uploadFile(file);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File: " + file + " was not found." + e);
+        }
+
     }
 }
--- src/main/java/no/ntnu/sjakkarena/services/FileUploadService.java
+++ src/main/java/no/ntnu/sjakkarena/services/FileUploadService.java
@@ -1,15 +1,21 @@
 package no.ntnu.sjakkarena.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileUploadService {
 
     public void uploadFile(MultipartFile file) throws IOException, IllegalStateException {
-        file.transferTo(new File("C:\\Sjakk-Arena\\endPositions\\" + file.getOriginalFilename()));
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get("C:\\Sjakk-Arena\\endPositions\\" + fileName);
+        Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
     }
 }
