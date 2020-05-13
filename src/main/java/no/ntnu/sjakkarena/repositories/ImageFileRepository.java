a4e15e3a1ac2a1358be111e6330b734342b2cb47
andremha <andremha@stud.ntnu.no>
2020-05-13 11:31:23 +0200

    Change insert image sql-query

--- src/main/java/no/ntnu/sjakkarena/repositories/ImageFileRepository.java
+++ src/main/java/no/ntnu/sjakkarena/repositories/ImageFileRepository.java
@@ -8,15 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 
@@ -30,28 +24,17 @@ public class ImageFileRepository {
 
     /**
      * Adds a image name to the database
+     *
      * @param image to be added
      */
     public void addNewImage(Image image) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
         try {
-            //Adapted code from https://stackoverflow.com/questions/12882874/how-can-i-get-the-autoincremented-id-when-i-insert-a-record-in-a-table-via-jdbct
-            jdbcTemplate.update(
-                    new PreparedStatementCreator() {
-                        public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                            PreparedStatement ps =
-                                    connection.prepareStatement(
-                                            "INSERT INTO `sjakkarena`.`image` " +
-                                                    "(playerId, gameId, fileName, time_Uploaded)" +
-                                            "VALUES (" + "'" + image.getPlayerId() + "', '" + image.getGameId() +
-                                                      "', '" + image.getFilename() + "', '" + image.getTimeUploaded() +
-                                                      "')", new String[]{"id"});
-                            return ps;
-                        }
-                    },
-                    keyHolder);
+            jdbcTemplate.update("INSERT INTO `sjakkarena`.`image` " +
+                    "(playerId, gameId, fileName, time_Uploaded)" +
+                    "VALUES (" + "'" + image.getPlayerId() + "', '" + image.getGameId() +
+                    "', '" + image.getFilename() + "', '" + image.getTimeUploaded() +
+                    "')");
         } catch (DataAccessException e) {
-            System.out.println(e.getMessage());
             throw new TroubleUpdatingDBException("Could not add image to database. Possible reasons: \n" +
                     "1. Image is already registered in database \n" +
                     "2. Name/value pairs in JSON are missing");
@@ -59,8 +42,10 @@ public class ImageFileRepository {
     }
 
     /**
-     * Find names of images belonging to specified game
+     * Return names of images belonging to specified game
+     *
      * @param gameId id of game to find images for
+     * @return images names of images belonging to specified game
      */
     public List<Image> findImagesToGameId(int gameId) {
         String sql = "select * from `sjakkarena`.`image`" +
