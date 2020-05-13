02028534d03981616e0a86866cd1e8151b8091b9
andremha <andremha@stud.ntnu.no>
2020-05-13 15:51:08 +0200

    Allow multiple images with the same name

--- src/main/java/no/ntnu/sjakkarena/services/FileStorageService.java
+++ src/main/java/no/ntnu/sjakkarena/services/FileStorageService.java
@@ -16,7 +16,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
-// adapted from: https://www.callicoder.com/spring-boot-file-upload-download-jpa-hibernate-mysql-database-example/ & https://spring.io/guides/gs/uploading-files/
+// adapted from: https://www.callicoder.com/spring-boot-file-upload-download-jpa-hibernate-mysql-database-example/ &
+// https://spring.io/guides/gs/uploading-files/
 
 @Service
 public class FileStorageService {
@@ -36,42 +37,81 @@ public class FileStorageService {
 
     /**
      * Saves a file to local and registers to database
-     * @param file file to save
+     *
+     * @param file     file to save
      * @param playerId id of player uploading.
      * @throws IOException
      * @throws IllegalStateException
      * @throws NullPointerException
      */
     public void saveFile(MultipartFile file, int playerId) throws IOException, IllegalStateException, NullPointerException {
-        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        String fileName = getFileName(file);
         try {
             // Check if name is invalid
             if (fileName.contains("..")) {
                 throw new FileSystemException("Invalid name!");
             }
-            // Adapted from: https://github.com/spring-guides/gs-uploading-files/pull/68/files
-            Path savePath = this.rootLocation.resolve(
-                    Paths.get(file.getOriginalFilename()))
-                    .normalize().toAbsolutePath();
+            Path savePath = provideUniquePath(file);
             if (!savePath.getParent().equals(this.rootLocation.toAbsolutePath())) {
                 // This is a security check
                 throw new StorageException("Cannot store file outside current directory.");
             }
-
             Files.copy(file.getInputStream(), savePath, StandardCopyOption.REPLACE_EXISTING);
-            fileToDB(fileName, playerId);
+            fileToDB(savePath.getFileName().toString(), playerId);
         } catch (IOException | NullPointerException e) {
             throw new IOException(e);
         }
     }
 
     /**
+     * Returns the specified file's name
+     *
+     * @param file
+     * @return the specified file's name
+     */
+    private String getFileName(MultipartFile file) {
+        return StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+    }
+
+    /**
+     * Returns a unique path for the specified file
+     *
+     * @param file
+     * @return a unique path for the specified file
+     */
+    private Path provideUniquePath(MultipartFile file) {
+        int i = 1;
+        String fileName = file.getOriginalFilename();
+        Path path = getSavePath(fileName);
+        while (Files.exists(path)) {
+            path = getSavePath("(" + i + ")" + fileName);
+            i++;
+        }
+        return path;
+    }
+
+
+    /**
+     * Return the path where the file with the given filename is saved
+     *
+     * @param filename
+     * @return the path where the file with the given filename is saved
+     */
+    private Path getSavePath(String filename) {
+        // Adapted from: https://github.com/spring-guides/gs-uploading-files/pull/68/files
+        return this.rootLocation.resolve(
+                Paths.get(filename))
+                .normalize().toAbsolutePath();
+    }
+
+    /**
      * Input file details to database
+     *
      * @param filename name of file
      * @param playerId id of player uploading file
      * @throws IOException
      */
-    private void fileToDB (String filename, int playerId) throws IOException {
+    private void fileToDB(String filename, int playerId) throws IOException {
         int gameId = playersGameService.getActiveGameId(playerId);
         String timeUploaded = LocalDateTime.now().toString();
         Image image = new Image(filename, gameId, playerId, timeUploaded);
@@ -80,6 +120,7 @@ public class FileStorageService {
 
     /**
      * Gets images belonging to game
+     *
      * @param gameId id of game to find images for.
      * @return list of images belonging to game
      */
@@ -90,6 +131,7 @@ public class FileStorageService {
 
     /**
      * Gets the path for folder to upload
+     *
      * @return path of folder
      */
     public String getPath() {
